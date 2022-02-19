c6285f534004eb4a463532a4e93804c0b47ed47e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-02-19 13:04:46 -0300

    refatoração do teste About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Teste do componente <About.js />.', () => {
   beforeEach(() => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    renderWithRouter(<App />, { route: '/about' });
   });
 
   test('Verifica se a página contém um heading h2 com o texto About Pokédex.', () => {
-    renderWithRouter(<App />, { route: '/about' });
-
     const textInAbout = screen
       .getByRole('heading', { name: /About Pokédex/i, level: 2 });
     expect(textInAbout).toBeInTheDocument();
   });
 
   test('Verifica se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    renderWithRouter(<App />, { route: '/about' });
-
     const phraseOnePartOne = screen.getByText(/This application simulates a Pokédex,/i);
     const phraseOnePartTwo = screen
       .getByText(/a digital encyclopedia containing all Pokémons/i);
@@ -36,7 +27,6 @@ describe('Teste do componente <About.js />.', () => {
   });
 
   test('Verifica se a página contém a imagem de uma Pokédex', () => {
-    renderWithRouter(<App />, { route: '/about' });
     const srcImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const img = screen.getByAltText(/Pokédex/i);
     expect(img.src === srcImg).toBe(true);
